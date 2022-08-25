2d6479b011285588a5df7bc1ad8aa203c6ee77e2
BethanCropp <43740410+BethanCropp@users.noreply.github.com>
2022-08-25 13:11:10 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,9 +6,10 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Goodbye": "World"}
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
